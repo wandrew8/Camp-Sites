e2aab7e48ba003a471fb7d5a964a429c30d505a5
Andrew Weiss <wandrew8@gmail.com>
2019-11-17 15:16:49 -0800

    navbar added

---
+++ public/assets/app.js
@@ -0,0 +1,22 @@
+var inputValue = $("#selectState :selected").val();
+let apiKey = "8qah6C7HinYsTe78kWFUYdGnfdvvHOPa1JPycDTC";
+let inputValue = document.getElementById("selectState").value;
+let queryURL = (`https://developer.nps.gov/api/v1/parks?stateCode=${inputValue}&api_key=${apiKey}`)
+
+$("#search-state").on("click", (event) => {
+    event.preventDefault();
+    console.log(inputValue)
+    displayCampgrounds();
+    console.log("Yes, it's working")
+})
+
+
+
+function displayCampgrounds() {
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+    });
+}
