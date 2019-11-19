0f2209b9ef1b02e6700c08dcaf0b09ee28639536
Andrew Weiss <wandrew8@gmail.com>
2019-11-19 12:15:52 -0800

    contact page form added

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,10 +1,11 @@
 var inputValue = $("#selectState :selected").val();
 let apiKey = "8qah6C7HinYsTe78kWFUYdGnfdvvHOPa1JPycDTC";
-let inputValue = document.getElementById("selectState").value;
+// let inputValue = document.getElementById("selectState").value;
 let queryURL = (`https://developer.nps.gov/api/v1/parks?stateCode=${inputValue}&api_key=${apiKey}`)
 
 $("#search-state").on("click", (event) => {
     event.preventDefault();
+    console.log("button is clicked")
     console.log(inputValue)
     displayCampgrounds();
     console.log("Yes, it's working")
