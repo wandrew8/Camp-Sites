7003006d2d675baa5d5a3764bb45da0ba57c7292
Andrew Weiss <wandrew8@gmail.com>
2019-11-19 13:23:50 -0800

    tables and cards added

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,6 +1,21 @@
-var inputValue = $("#selectState :selected").val();
+import 'ol/ol.css';
+import { Map, View } from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import { OSM } from 'ol/source';
+
+const map = new Map({
+  layers: [new TileLayer({
+    source: new OSM()
+  })],
+  target: 'map',
+  view: new View({
+    center: [0, 0],
+    zoom: 2
+  })
+});
+
+var inputValue = "WA"
 let apiKey = "8qah6C7HinYsTe78kWFUYdGnfdvvHOPa1JPycDTC";
-// let inputValue = document.getElementById("selectState").value;
 let queryURL = (`https://developer.nps.gov/api/v1/parks?stateCode=${inputValue}&api_key=${apiKey}`)
 
 $("#search-state").on("click", (event) => {
@@ -21,3 +36,7 @@ function displayCampgrounds() {
         console.log(response)
     });
 }
+
+
+
+
