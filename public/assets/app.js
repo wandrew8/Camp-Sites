878014d5ccea64ce1b5fd4ce5b472a5a42ba6687
Andrew Weiss <wandrew8@gmail.com>
2019-12-01 10:45:32 -0800

    play/pause button added to carousel

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,19 +1,3 @@
-import 'ol/ol.css';
-import { Map, View } from 'ol';
-import TileLayer from 'ol/layer/Tile';
-import { OSM } from 'ol/source';
-
-const map = new Map({
-  layers: [new TileLayer({
-    source: new OSM()
-  })],
-  target: 'map',
-  view: new View({
-    center: [0, 0],
-    zoom: 2
-  })
-});
-
 var inputValue = "WA"
 let apiKey = "8qah6C7HinYsTe78kWFUYdGnfdvvHOPa1JPycDTC";
 let queryURL = (`https://developer.nps.gov/api/v1/parks?stateCode=${inputValue}&api_key=${apiKey}`)
@@ -25,8 +9,6 @@ $("#search-state").on("click", (event) => {
     displayCampgrounds();
     console.log("Yes, it's working")
 })
-
-
 
 function displayCampgrounds() {
     $.ajax({
