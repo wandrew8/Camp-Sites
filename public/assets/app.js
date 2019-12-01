f7f55bcd7b0ba897b14069f2e7e4d8f662f06637
Andrew Weiss <wandrew8@gmail.com>
2019-12-01 12:01:13 -0800

    search page with functioning api call added

--- public/assets/app.js
+++ public/assets/app.js
@@ -14,11 +14,32 @@ function displayCampgrounds() {
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function (response) {
-        console.log(response)
+    }).then(function (results) {
+        displayCards(results)
     });
 }
 
+function displayCards(results) {
+    console.log(results.data)
+    console.log(results.data[0].name)
 
+    let campSiteCardConatiner = document.getElementById("camp-container");
+    for (let i = 0; i < results.data.length; i++) {
+        campSiteCardConatiner.innerHTML += 
+        `<div class="col-xs-12 col-sm-6 col-lg-4">
+        <div class="card camp-card my-3">
+            <div class="card-body">
+              <h2 class="card-title">${results.data[i].name}</h2>
+              <hr>
+              <p class="byline">${results.data[i].designation}</small>
+                <p class="card-text">${results.data[i].description}</p>
+            </div>
+          <div class="text-center pb-3">
+            <a href="${results.data[i].url}" target="_blank" class="btn btn-primary">Visit Website!<i class="fas pl-2 fa-share-square"></i></a>
+          </div>
+        </div>
+      </div>`
+    }
+}
 
 
